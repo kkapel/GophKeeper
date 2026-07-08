3e92bd7a747fb608236ec0a4c6cc21a2d61c5838
Grigorii Vakhnin <>
2026-07-08 16:47:53 +0300

    Добавлен логгер

--- internal/server/run.go
+++ internal/server/run.go
@@ -1,11 +1,24 @@
 package server
 
-import "github.com/kkapel/gophkeeper/internal/db/connections"
+import (
+	"context"
+	"log/slog"
+	"time"
+
+	"github.com/kkapel/gophkeeper/internal/db/connections"
+	"github.com/kkapel/gophkeeper/internal/logger"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
+)
 
 func Run() error {
-	var dbstr string = ""
+	// Логгер
+	if err := logger.Initialize("INFO"); err != nil {
+		return err
+	}
 
 	// Инициализация БД
+	var dbstr string = ""
 	db, err := connections.InitDB(dbstr, "./migrations")
 	if err != nil {
 		return err
@@ -21,4 +34,27 @@ func Run() error {
 	}
 
 	return nil
+}
+
+func LoggingInterceptor(
+	ctx context.Context,
+	req any,
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler,
+) (any, error) {
+	start := time.Now()
+
+	// вызываем сам обработчик (аналог h.ServeHTTP)
+	resp, err := handler(ctx, req)
+
+	duration := time.Since(start)
+	code := status.Code(err) // gRPC-код из ошибки (OK, если err == nil)
+
+	logger.Log.Info("incoming gRPC request",
+		slog.String("method", info.FullMethod), // аналог r.URL.Path
+		slog.Duration("duration", duration),
+		slog.String("code", code.String()), // аналог статуса ответа
+	)
+
+	return resp, err
 }
